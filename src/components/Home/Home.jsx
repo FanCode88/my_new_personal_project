ca78383bb01f1d8d4502656f00419f21b3a8ff31
Saceanu Ionut <saceanu.ionut@icloud.com>
2024-12-15 15:01:25 +0200

    add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,69 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './home.scss';
 
 const Home = () => {
     return (
         <div className="home">
-            <div className="homeContainer">
-                <h1>Welcome to our page!</h1>
-                <ul>
+            <motion.div
+                className="homeContainer"
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+            >
+                <motion.h1
+                    initial={{ scale: 0.8, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{ delay: 0.3, duration: 0.8 }}
+                >
+                    Welcome to our page!
+                </motion.h1>
+
+                <motion.ul
+                    className="navLinks"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.5, staggerChildren: 0.2 }}
+                >
                     <div className="container">
-                        <li>
+                        <motion.li
+                            whileHover={{ scale: 1.2, rotate: 3 }}
+                            transition={{ type: "spring", stiffness: 300 }}
+                        >
                             <Link to="/services">Services</Link>
-                        </li>
-                        <li>
+                        </motion.li>
+                        <motion.li
+                            whileHover={{ scale: 1.2, rotate: 3 }}
+                            transition={{ type: "spring", stiffness: 300 }}
+                        >
                             <Link to="/about">About</Link>
-                        </li>
-                        <li>
+                        </motion.li>
+                        <motion.li
+                            whileHover={{ scale: 1.2, rotate: 3 }}
+                            transition={{ type: "spring", stiffness: 300 }}
+                        >
                             <Link to="/contact">Contact</Link>
-                        </li>
+                        </motion.li>
                     </div>
-                </ul>
+                </motion.ul>
+
                 <div className="wrapper">
-                    <div className="formboxLogin">
+                    <motion.div
+                        className="formboxLogin"
+                        whileHover={{ scale: 1.1, rotate: 10 }}
+                        transition={{ duration: 0.4 }}
+                    >
                         <Link to="/login">
-                            <button>JOIN US</button>
+                            <motion.button
+                                whileHover={{ backgroundColor: "#ff758c", scale: 1.1 }}
+                                transition={{ type: "tween", duration: 0.3 }}
+                            >
+                                JOIN US
+                            </motion.button>
                         </Link>
-                    </div>
+                    </motion.div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 };
