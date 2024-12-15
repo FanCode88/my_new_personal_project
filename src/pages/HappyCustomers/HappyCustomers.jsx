7f967ed019e960999c717fe06c4abfed72621251
Saceanu Ionut <saceanu.ionut@icloud.com>
2024-12-15 19:32:05 +0200

    add

--- src/pages/HappyCustomers/HappyCustomers.jsx
+++ src/pages/HappyCustomers/HappyCustomers.jsx
@@ -1,22 +1,42 @@
 import './happyCustomers.scss';
-import happyCostomer from '../../../public/assets/man.jpg';
+import { useState } from 'react';
+import customers from '../../pages/HappyCustomers/data';
 
 const HappyCustomers = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % customers.length);
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex(
+            (prevIndex) => (prevIndex - 1 + customers.length) % customers.length
+        );
+    };
+
+    const { img, name, feedback, username, title } = customers[currentIndex];
+
     return (
         <div className="happyCustomers">
             <div className="container">
                 <div className="boxMain">
-                    <h1>OUR HAPPY CUSTOMER</h1>
+                    <h1>{title}</h1>
                     <div className="line"></div>
                     <div className="circle">
-                        <img src={happyCostomer} alt="Tim Roberts" />
+                        <img src={img} alt={name} />
                     </div>
-                    <h2>Tim Roberts</h2>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Minima aliquam earum quos ut, sit nemo recusandae?
-                    </p>
-                    <div className="foot">@Tim Roberts</div>
+                    <h2>{name}</h2>
+                    <p>{feedback}</p>
+                    <div className="foot">{username}</div>
+                    <div className="navigation">
+                        <button onClick={handlePrev} className="prev">
+                            &lt;
+                        </button>
+                        <button onClick={handleNext} className="next">
+                            &gt;
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/pages/HappyCustomers/data.js
@@ -0,0 +1,30 @@
+const customers = [
+    {
+        id: 1,
+        title: 'OUR HAPPY CUSTOMERS',
+        img: 'https://plus.unsplash.com/premium_photo-1663047504447-d54e624ef2d5?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHVzZXJ8ZW58MHx8MHx8fDA%3D',
+        name: 'Roberta',
+        feedback:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima aliquam earum quos ut, sit nemo recusandae?',
+        username: '@Roberta',
+    },
+    {
+        id: 2,
+        title: 'OUR HAPPY CUSTOMERS',
+        img: 'https://plus.unsplash.com/premium_photo-1670071482460-5c08776521fe?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D',
+        name: 'Anna Smith',
+        feedback:
+            'This service is amazing! I will definitely recommend it to my friends.',
+        username: '@AnnaSmith',
+    },
+    {
+        id: 3,
+        title: 'OUR HAPPY CUSTOMERS',
+        img: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHVzZXJ8ZW58MHx8MHx8fDA%3D',
+        name: 'John and Mary',
+        feedback: 'We are so happy with the results! Totally worth it.',
+        username: '@JohnAndMary',
+    },
+];
+
+export default customers;
